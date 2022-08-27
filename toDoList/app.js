8b9fda4a1e831087580f5cbbd8fed6c21c24a3ed
Chen <oliviachen797@gmail.com>
2022-08-27 18:49:52 +1000

    <% ejs %>

--- toDoList/app.js
+++ toDoList/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
+const port = 3000;
 
 //register view engine we want to use ejs as our view engine of choicefor this application
 app.set("view engine", "ejs");
 
-const port = 3000;
 
 app.get("/", (req, res) => {
     // res.send("<h1>Hello lafeiki</h1>");
